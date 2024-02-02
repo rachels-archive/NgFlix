18d46e17cdcfcfb6460652409d5607a1c16cdec6
rachels-archive <rachelhui@protonmail.com>
2024-02-02 10:37:49 +0800

    Add movies service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { HeaderComponent } from './shared/header/header.component'
+import { FooterComponent } from './shared/footer/footer.component'
+import { HomeComponent } from './pages/home/home.component'
+import { MovieListComponent } from './pages/movie-list/movie-list.component'
 import { SliderComponent } from './components/slider/slider.component'
+import { HttpClientModule } from '@angular/common/http'
+import { MoviesService } from './services/movies.service'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MovieListComponent, SliderComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MovieListComponent,
+    SliderComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [MoviesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { MoviesService } from '../../services/movies.service'
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+  styleUrl: './slider.component.scss',
 })
 export class SliderComponent {
+  constructor(private moviesService: MoviesService) {}
 
+  movies$ = this.moviesService.getPopularMovies()
 }
