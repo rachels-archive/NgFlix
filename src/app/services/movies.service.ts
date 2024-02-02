a767afa247bc3dc0142b89e9c2e9c5138f5e8677
rachels-archive <rachelhui@protonmail.com>
2024-02-02 10:49:02 +0800

    Define type Movie and MovieResponse

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { MovieResponse } from '../types/movie'
 
 @Injectable()
 export class MoviesService {
   constructor(private http: HttpClient) {}
 
   getPopularMovies() {
-    return this.http.get<any>(
+    return this.http.get<MovieResponse>(
       'https://api.themoviedb.org/3/movie/popular?api_key=ed1c2f9f17ef7e702b4a97422c78ca9d'
     )
   }
