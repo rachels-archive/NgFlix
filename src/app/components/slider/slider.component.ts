2c6d00fa6403f540489613730731a2a944c89822
rachels-archive <rachelhui@protonmail.com>
2024-02-02 11:37:04 +0800

    Load slides every 5 second interval

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -6,8 +6,16 @@ import { MoviesService } from '../../services/movies.service'
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
   constructor(private moviesService: MoviesService) {}
 
   movies$ = this.moviesService.getPopularMovies()
+
+  slideIndex = 0
+
+  ngOnInit() {
+    setInterval(() => {
+      this.slideIndex++
+    }, 5000)
+  }
 }
